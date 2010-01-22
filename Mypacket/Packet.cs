2eb350f11560ff3e97bf34f7fc578e210e98861b
longjourney <a29855378@hotmail.com>
2010-01-22 11:27:08 +0800

    add packet files

---
+++ Mypacket/Packet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mypacket
+{
+    public class Packet
+    {
+        [Serializable]
+        public class packet
+        {
+
+            public string Cmd
+            {
+                get { return cmd; }
+            }
+            private string cmd;
+
+            public int Move
+            {
+                get { return move; }
+            }
+            private int move;
+
+            public string Msg
+            {
+                get { return msg; }
+            }
+            private string msg;
+
+            public string Name
+            {
+                get { return name; }
+            }
+            private string name;
+
+            public packet(string cmd, string name, string msg, int move)
+            {
+                this.cmd = cmd;
+                this.name = name;
+                this.msg = msg;
+                this.move = move;
+            }
+        }
+    }
+}
